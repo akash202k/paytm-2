8f2097718338577218fecc09a39315a17e1362ce
Akash Pawar <akashpawar202k@gmail.com>
2024-02-19 23:07:20 +0530

    update

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import zodSchema from "../zod/schema"
 import HttpStatusCode from "../util/statusCode";
-import { PrismaClient, User } from "@prisma/client";
+import { Account, PrismaClient, User } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import secret from "../util/config";
 import auth from "../middleware/auth";
@@ -15,41 +15,57 @@ router.get('/', (req, res) => {
     res.send("User Health Ok !");
 })
 
+
+// signIn
 router.post('/signup', async (req, res) => {
-    const validation = zodSchema.userSignUp.safeParse(req.body);
-    if (!validation.success) {
-        console.log(validation.error.issues);
-        res.status(HttpStatusCode.CONFLICT).json(validation.error.issues);
-        return;
-    }
 
-    // check either user exist with same username
-    const isUserExist = await prisma.user.findUnique({ where: { username: req.body.username } });
+    await prisma.$transaction(async (prisma) => {
+        const validation = zodSchema.userSignUp.safeParse(req.body);
+        if (!validation.success) {
+            console.log(validation.error.issues);
+            res.status(HttpStatusCode.CONFLICT).json(validation.error.issues);
+            return;
+        }
 
-    if (isUserExist) {
-        console.log(`${req.body.username} alredy exist.`);
-        res.status(HttpStatusCode.CONFLICT).json({
-            message: "Email already taken / Incorrect inputs"
+        // check either user exist with same username
+        const isUserExist = await prisma.user.findUnique({ where: { username: req.body.username } });
+
+        if (isUserExist) {
+            console.log(`${req.body.username} alredy exist.`);
+            res.status(HttpStatusCode.CONFLICT).json({
+                message: "Email already taken / Incorrect inputs"
+            })
+            return;
+        }
+
+        const newUser: User = await prisma.user.create({
+            data: req.body
         })
-        return;
-    }
 
-    const newUser: User = await prisma.user.create({
-        data: req.body
+        // create account in account table now 
+        const newAccount: Account = await prisma.account.create({
+            data: { userId: newUser.id }
+        })
+
+        console.log("new account created \n", newAccount);
+
+        console.log(newUser);
+
+        const jwtToken: string = jwt.sign({ id: newUser.id }, secret);
+        const token: string = `Bearer ${jwtToken}`;
+
+        res.status(HttpStatusCode.OK).json({
+            message: "User created successfully",
+            token: token
+        })
+
     })
 
-    console.log(newUser);
-
-    const jwtToken: string = jwt.sign({ id: newUser.id }, secret);
-    const token: string = `Bearer ${jwtToken}`;
-
-    res.status(HttpStatusCode.OK).json({
-        message: "User created successfully",
-        token: token
-    })
 
 })
 
+
+// signIn
 router.post('/signin', auth, async (req: IRequest, res: Response) => {
 
     const userId = req.userId;
@@ -73,6 +89,8 @@ router.post('/signin', auth, async (req: IRequest, res: Response) => {
     }
 })
 
+
+// update
 router.put('/', auth, async (req: IRequest, res: Response) => {
     const validation = zodSchema.updateUser.safeParse(req.body);
     if (!validation.success) {
@@ -92,7 +110,6 @@ router.put('/', auth, async (req: IRequest, res: Response) => {
         message: "User Info Updated",
         User: updateUser
     })
-
 })
 
 
